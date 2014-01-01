68f948de9d1428d16e96a7d30dd9eb2f201b6fae
amadornes <amadornes@gmail.com>
2014-01-01 02:19:22 +0100

    Github problems...

--- src/es/amadornes/openlauncher/OpenLauncher.java
+++ src/es/amadornes/openlauncher/OpenLauncher.java
@@ -4,8 +4,10 @@ import java.awt.Font;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import es.amadornes.openlauncher.api.gui.ComponentFancyButton1;
+import net.minecraft.login.MojangAuth;
+import es.amadornes.openlauncher.api.gui.ComponentFancyButton;
 import es.amadornes.openlauncher.api.gui.server.DownloadServer;
 import es.amadornes.openlauncher.gui.GUI;
 import es.amadornes.openlauncher.gui.TabConsole;
@@ -31,14 +33,16 @@ public class OpenLauncher {
 	public static GUI gui;
 	
 	/* Set some variables */
-	public static boolean loggedIn = true;
-	public static String username = "amadornes"; // TODO: Remove static username and load it from "lastlogin" or from login form
+	public static boolean loggedIn = false;
+	public static String username = "Unknown";
 	public static Font font;
 	
 	private static DownloadServer[] servers = new DownloadServer[]{ new DownloadServerAmadornes() };
 	public static List<Modpack> modpacks = new ArrayList<Modpack>();
 	
 	public static void main(String[] args){
+		/* Add shutdown event */
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {public void run(){ exit(); }}));
 		
 		preInit();
 		
@@ -54,13 +58,13 @@ public class OpenLauncher {
 		gui = new GUI(1280, 720);
 		
 		/* Add sidebar Tabs */
-		gui.addComponent(new ComponentFancyButton1(29 - gui.insets.left, 659 - gui.insets.top - 45 - 45 - 45, 150, 40,	"News").setSelected(true));
+		gui.addComponent(new ComponentFancyButton(29 - gui.insets.left, 659 - gui.insets.top - 45 - 45 - 45, 150, 40, "News").setSelected(true));
 		gui.addTab(new TabNews(gui), 0);
-		gui.addComponent(new ComponentFancyButton1(29 - gui.insets.left, 659 - gui.insets.top - 45 - 45, 150, 40,		"Modpacks").setTab(1));
+		gui.addComponent(new ComponentFancyButton(29 - gui.insets.left, 659 - gui.insets.top - 45 - 45, 150, 40, "Modpacks").setTab(1));
 		gui.addTab(new TabModpacks(gui), 1);
-		gui.addComponent(new ComponentFancyButton1(29 - gui.insets.left, 659 - gui.insets.top - 45, 150, 40,			"Console").setTab(2));
+		gui.addComponent(new ComponentFancyButton(29 - gui.insets.left, 659 - gui.insets.top - 45, 150, 40, "Console").setTab(2));
 		gui.addTab(new TabConsole(gui), 2);
-		gui.addComponent(new ComponentFancyButton1(29 - gui.insets.left, 659 - gui.insets.top, 150, 40,					"Settings").setTab(3));
+		gui.addComponent(new ComponentFancyButton(29 - gui.insets.left, 659 - gui.insets.top, 150, 40,	"Settings").setTab(3));
 		gui.addTab(new TabSettings(gui), 3);
 		
 		init();
@@ -68,9 +72,6 @@ public class OpenLauncher {
 		/* Center and show the GUI */
 		gui.center();
 		gui.show();
-		
-		/* Load Modpacks */
-		loadModpacks();
 		
 		postInit();
 	}
@@ -96,6 +97,9 @@ public class OpenLauncher {
 		Util.getWorkingDirectory().mkdirs();
 		Util.getInstancesFolder().mkdirs();
 		Util.getDownloadsFolder().mkdirs();
+		
+		/* Try to login using the LastLogin function */
+		LastLogin.tryLoading();
 	}
 	
 	private static void init(){
@@ -103,14 +107,43 @@ public class OpenLauncher {
 	}
 	
 	private static void postInit(){
-		
+		/* Start loading modpacks from the various servers */
+		loadModpacks();
 	}
+	
+	private static void exit(){}
 	
 	public static Modpack getPack(String server, String id){
 		for(Modpack m : modpacks)
 			if(m.getId().equals(server + "_" + id))
 				return m;
 		return null;
+	}
+	
+	public static void setLoggedIn(boolean logged){
+		gui.labelUser.setVisible(!logged);
+		gui.labelPass.setVisible(!logged);
+		gui.user.setVisible(!logged);
+		gui.pass.setVisible(!logged);
+		loggedIn = logged;
+	}
+	
+	public static void login(){
+		Map<String, String> data = null;
+		try {
+			data = MojangAuth.authenticate(gui.user.getText(), new String(gui.pass.getPassword()));
+
+			LastLogin.ACCESS_TOKEN = data.get("accessToken");
+			LastLogin.CLIENT_TOKEN = data.get("clientToken");
+			LastLogin.UUID = data.get("UUID");
+			username = data.get("username");
+		} catch (Exception e) {}
+		
+		if(data != null){
+			setLoggedIn(true);
+			LastLogin.save();
+			gui.removeComponent(gui.loginButton);
+		}
 	}
 	
 }
